cab3b591130138fd5c105ca5bf4284780dfc8201
Santiago Lezica <slezica89@gmail.com>
2015-07-11 19:55:19 -0300

    Initial commit. Left out Builds and Temp

---
+++ BoomCube/Assets/Scripts/BoomScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoomScript : MonoBehaviour {
+
+	public int interval = 1;
+	public float deltaMultiplier = 1;
+	public int maxBooms = 3;
+
+	private float lastBoom;
+	private int boomCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		lastBoom = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.realtimeSinceStartup - lastBoom >= interval) {
+			if (++boomCount == maxBooms + 1) {
+				Destroy(gameObject);
+				return;
+			}
+
+			lastBoom = Time.realtimeSinceStartup;
+
+			Vector3 delta = new Vector3(
+				Random.value * deltaMultiplier,
+				Random.value * deltaMultiplier,
+				Random.value * deltaMultiplier
+			);
+
+			Instantiate(this.gameObject, this.transform.position + delta, this.transform.rotation);
+			this.transform.position -= delta;
+
+			print("Boom!");
+		}
+	}
+}
---
+++ BoomCube/Assets/Scripts/whiteCubeScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whiteCubeScript : MonoBehaviour {
+
+	public float movementAngle  = 45;
+	public float movementForce  = 5;
+	public float movementRepeat = 1; // seconds
+
+	private Vector3 directionLeft  = Quaternion.AngleAxis (360 - 45, Vector3.forward) * Vector3.left;
+	private Vector3 directionRight = Quaternion.AngleAxis (45, Vector3.forward) * Vector3.right;
+
+	private Vector3 movementDir;
+	private bool isFalling = true;
+	
+	void Start () {
+		spin ();
+		InvokeRepeating ("jump", 1f, movementRepeat);
+	}
+	
+	void Update () {
+		// Nada
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Floor" && isFalling) {
+			isFalling = false;
+			spin ();
+		}
+
+		if (collision.gameObject.tag == "Wall")
+			bounce();
+	}
+//
+//	void OnCollisionStay(Collision collision) {
+//		if (collision.gameObject.tag == "Wall")
+//			bou
+//	}
+	
+	void jump() {
+		GetComponent<Rigidbody>().AddForce (movementDir * movementForce);
+	}
+
+	void bounce() {
+		spin ();
+	}
+
+	void spin() {
+		print ("X" + Random.value);
+		print ("X" +Random.value);
+		
+		movementDir =  new Vector3(Random.value - 0.5f, 1, Random.value - 0.5f).normalized;
+		print ("Moving to " + movementDir);
+//		float newAngle = newDir.x > newDir.y ? 45 : (360 - 45);
+//		movementDir = Quaternion.AngleAxis (360 - 45, Vector3.forward) * Vector3.left;
+	}
+}
