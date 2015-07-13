20d77f529df16275bc2a13fbe493efcd20683ff6
Santiago Lezica <slezica89@gmail.com>
2015-07-12 23:09:14 -0300

    Jump, gaze, shoot!

---
+++ BoomCube/Assets/Scripts/groundScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class groundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onRayHit() {
+		print ("Ground/wall hit");
+	}
+}
--- BoomCube/Assets/Scripts/whiteCubeScript.cs
+++ BoomCube/Assets/Scripts/whiteCubeScript.cs
@@ -7,6 +7,9 @@ public class whiteCubeScript : MonoBehaviour {
 	public float movementForce  = 5;
 	public float movementRepeat = 1; // seconds
 
+	public Material defaultMaterial;
+	public Material selectedMaterial;
+
 	private Vector3 directionLeft  = Quaternion.AngleAxis (360 - 45, Vector3.forward) * Vector3.left;
 	private Vector3 directionRight = Quaternion.AngleAxis (45, Vector3.forward) * Vector3.right;
 
@@ -14,6 +17,8 @@ public class whiteCubeScript : MonoBehaviour {
 	private bool isFalling = true;
 	
 	void Start () {
+		GetComponent<Renderer> ().material = defaultMaterial;
+
 		spin ();
 		InvokeRepeating ("jump", 1f, movementRepeat);
 	}
@@ -31,6 +36,18 @@ public class whiteCubeScript : MonoBehaviour {
 		if (collision.gameObject.tag == "Wall")
 			bounce();
 	}
+
+	public void onGazeEnter() {
+		GetComponent<Renderer> ().material = selectedMaterial;
+	}
+
+	public void onGazeLeave() {
+		GetComponent<Renderer> ().material = defaultMaterial;
+	}
+
+	public void onHit() {
+		clone ();
+	}
 //
 //	void OnCollisionStay(Collision collision) {
 //		if (collision.gameObject.tag == "Wall")
@@ -45,13 +62,11 @@ public class whiteCubeScript : MonoBehaviour {
 		spin ();
 	}
 
-	void spin() {
-		print ("X" + Random.value);
-		print ("X" +Random.value);
-		
+	void spin() {		
 		movementDir =  new Vector3(Random.value - 0.5f, 1, Random.value - 0.5f).normalized;
-		print ("Moving to " + movementDir);
-//		float newAngle = newDir.x > newDir.y ? 45 : (360 - 45);
-//		movementDir = Quaternion.AngleAxis (360 - 45, Vector3.forward) * Vector3.left;
+	}
+
+	void clone() {
+		Instantiate (this);
 	}
 }
